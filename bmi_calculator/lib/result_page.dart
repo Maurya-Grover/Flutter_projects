5679c6a5fc0c5bb47cbe5a04c3c38632e2ce60dd
Maurya-Grover <maurya.grover@gmail.com>
2021-03-23 06:34:01 +0530

    random updates

--- bmi_calculator/lib/result_page.dart
+++ bmi_calculator/lib/result_page.dart
@@ -1,5 +1,5 @@
 import 'package:bmi_calculator/resources.dart';
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'constants.dart';
 
